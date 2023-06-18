0d6173a324e2d564d822681268ae2ea137e2575d
alibarmonster <alibarmonster@pop-os.localdomain>
2023-06-18 20:03:34 +0700

    feat: add dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/models/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     return {
       msg: 'This action return signup',
