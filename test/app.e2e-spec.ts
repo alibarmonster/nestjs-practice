480b6d5038ef957a964b534c3f9c593c95cd0738
alibarmonster <alibar838@gmail.com>
2023-06-21 16:06:12 +0700

    feat: add user e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/models/prisma.service';
 import * as pactum from 'pactum';
 import { AuthDto } from '../src/auth/dto';
+import { EditUserDto } from '../src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -96,6 +97,41 @@ describe('App e2e', () => {
           .expectStatus(200)
           .stores('userAt', 'access_token');
       });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('Edit user', () => {
+    const dto: EditUserDto = {
+      email: 'alibarmonster@gmail.com',
+      firstName: 'alibar',
+      lastName: 'monster',
+    };
+    it('should edit user', () => {
+      return pactum
+        .spec()
+        .patch('/users')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .withBody(dto)
+        .expectStatus(200)
+        .expectBodyContains(dto.email)
+        .expectBodyContains(dto.firstName)
+        .inspect();
     });
   });
 });
